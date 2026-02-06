ee92d487848f7727c4b54cea89fda00e6bb0097d
popovychvanja <85509224+popovychvanja@users.noreply.github.com>
2026-02-06 14:25:22 -0600

    Add base template, home page, dark/light toggle

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,17 +1,8 @@
+from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-from django.shortcuts import render  # if using templates
-
-def home(request):
-    
-    return render(request, 'home.html', {'title': "Vanja's Site"})
- 
-
-def about(request):
-    return HttpResponse("<h1>About Vanja</h1><p>I live in Portage la Prairie, Manitoba, and I'm learning Django + deployment!</p>")
+from .views import home
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('about/', about, name='about'),
-    # path('admin/', admin.site.urls),
 ]
