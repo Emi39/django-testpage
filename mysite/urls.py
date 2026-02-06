ca4a4941c6f8c99d52f95ebc53f8f40a2ad340f7
popovychvanja <85509224+popovychvanja@users.noreply.github.com>
2026-02-06 13:59:45 -0600

    Add about page

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,22 @@
-from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse  # For quick test
+from django.http import HttpResponse
+from django.shortcuts import render  # if using templates
 
 def home(request):
-    return HttpResponse("<h1>Hello from Vanja's site on Koyeb! 🚀</h1><p>Running Django 6.0</p>")
+    return HttpResponse("""
+    <h1>Hello from Vanja's site on Koyeb! 🚀</h1>
+    <p>Running Django 6.0</p>
+    <ul>
+        <li><a href="/about/">About me</a></li>
+        <li><a href="/contact/">Contact</a></li>
+    </ul>
+    """)
+
+def about(request):
+    return HttpResponse("<h1>About Vanja</h1><p>I live in Portage la Prairie, Manitoba, and I'm learning Django + deployment!</p>")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home, name='home'),  # Root URL '/'
+    path('', home, name='home'),
+    path('about/', about, name='about'),
+    # path('admin/', admin.site.urls),
 ]
