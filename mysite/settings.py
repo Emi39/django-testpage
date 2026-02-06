e1c65f2c229dbe8eabee0056c6c55161e18f1a49
popovychvanja <85509224+popovychvanja@users.noreply.github.com>
2026-02-06 13:51:27 -0600

    Fix DisallowedHost: add ALLOWED_HOSTS for custom domain

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&b1w+p)=oj+_r(sgo15jt1y#_n&k-czl$3#h83)*pphw)_xfy4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
